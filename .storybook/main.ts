bff3c6c03b071deac05d89db2bdb6c09cbef4dd6
umarmamatov <mamatov.umar@mail.ru>
2024-11-29 12:36:13 +0500

    add github pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,45 +1,24 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
-import { mergeConfig } from 'vite';
-import { resolve } from 'path';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
   },
-  viteFinal: async (config) => {
-    return mergeConfig(config, {
-      resolve: {
-        alias: {
-          vue: 'vue/dist/vue.esm-bundler.js',
-          '@': resolve(__dirname, '../src')
-        },
-        dedupe: ['vue']
-      },
-      css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: `@use "@/assets/colors" as *;`
-          }
-        }
-      },
-      optimizeDeps: {
-        include: [
-          'vue',
-          'primevue/password',
-          'primevue/inputtext',
-          'primevue/inputmask',
-          'primevue/floatlabel'
-        ]
-      }
-    });
-  }
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal: (config) => {
+    // Добавляем базовый путь для GitHub Pages
+    config.base = process.env.NODE_ENV === 'production' ? '/gts-ui/' : '/';
+    return config;
+  },
 };
+
 export default config;
