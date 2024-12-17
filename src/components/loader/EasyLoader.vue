3316ef14047133a5744d14ece1333673a1479d05
umarmamatov <mamatov.umar@mail.ru>
2024-12-17 11:21:41 +0500

    fix: easy loader

--- src/components/loader/EasyLoader.vue
+++ src/components/loader/EasyLoader.vue
@@ -3,43 +3,27 @@ import './EasyLoader.scss';
 import {computed} from "vue";
 
 const props = withDefaults(defineProps<{
-  size?: string
-  borderWidth?: string
+  size?: number
+  borderWidth?: number
   borderColor?: string
   borderTopColor?: string
 }>(), {
-  size: 'large',
+  size: 40,
+  borderWidth: 3,
   borderColor: 'var(--alpha-neutral200a)',
   borderTopColor: 'var(--grey-500)'
 })
 
-function chooseSizes(enterSize) {
-  switch (enterSize) {
-    case 'xsmall':
-      return {size: 8, borderWidth: 1}
-    case 'small':
-      return {size: 16, borderWidth: 2}
-    case 'medium':
-      return {size: 24, borderWidth: 3}
-    case 'large':
-      return {size: 48, borderWidth: 4}
-    case 'xlarge':
-      return {size: 96, borderWidth: 5}
-    default :
-      return {size: enterSize, borderWidth: Math.floor(enterSize * 0.1) || 1}
-  }
-}
-
 const style = computed(() => ({
-  width: `${chooseSizes(props.size).size}px`,
-  height: `${chooseSizes(props.size).size}px`,
-  'border-width': props.borderWidth ? `${props.borderWidth}px` : `${chooseSizes(props.size).borderWidth}px`,
-  'border-color': props.borderColor,
-  'border-top-color': props.borderTopColor
+  width: props.size + 'px',
+  height: props.size + 'px',
+  borderWidth: props.borderWidth + 'px',
+  borderColor: props.borderColor,
+  borderTopColor: props.borderTopColor
 }));
+
 </script>
 
 <template>
   <div class="loader" :style="style"></div>
-  {{ borderWidth }}
 </template>
