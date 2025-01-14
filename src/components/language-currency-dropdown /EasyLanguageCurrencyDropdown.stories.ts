7647c8bbf816208d50d5fec10ac5f476327b7b8a
alitolibov <alitolibov066@gmail.com>
2025-01-14 15:53:14 +0500

    feature/added EasyLanguageCurrencyDropdown.vue

--- src/components/language-currency-dropdown /EasyLanguageCurrencyDropdown.stories.ts
+++ src/components/language-currency-dropdown /EasyLanguageCurrencyDropdown.stories.ts
@@ -1,14 +1,62 @@
-import type {Meta, StoryObj} from '@storybook/vue3';
+import type {StoryObj} from '@storybook/vue3';
 import EasyLanguageCurrencyDropdown from "./EasyLanguageCurrencyDropdown.vue";
 
 const meta = {
   title: 'Components/EasyLanguageCurrencyDropdown',
   component: EasyLanguageCurrencyDropdown,
-  tags: ['autodocs']
+  tags: ['autodocs'],
+  argTypes: {
+    currencies: {
+      control: 'object',
+      description: 'Currencies',
+    }
+  }
 }
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {
+    currencies: [
+      {
+        base: 'EUR',
+        ru: 'Евро',
+        bool: true,
+        disabled: false
+      },
+      {
+        base: 'UAH',
+        ru: 'Гривны',
+        bool: true,
+        disabled: false
+      },
+      {
+        base: 'KZT',
+        ru: 'Тенге',
+        bool: true,
+        disabled: true
+      },
+      {
+        base: 'AZN',
+        ru: 'Манат',
+        bool: true,
+        disabled: false
+      },
+      {
+        base: 'HKD',
+        ru: 'Гонконгские доллары',
+        bool: false,
+        disabled: false
+      },
+      {
+        base: 'UZS',
+        ru: 'Узбекские сумы',
+        bool: true,
+        disabled: true
+      },
+    ],
+    locale: 'ru',
+    currency: 'EUR'
+  }
 }
