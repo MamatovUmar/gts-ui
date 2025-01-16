5d2c28d021f378438f7cacddc218de596e31c8f9
umarmamatov <mamatov.umar@mail.ru>
2025-01-16 10:33:35 +0500

    textarea refactoring

--- src/components/textarea/EasyTextarea.vue
+++ src/components/textarea/EasyTextarea.vue
@@ -1,49 +1,45 @@
 <script setup lang="ts">
-import Textarea from "primevue/textarea";
-import "./EasyTextarea.scss"
+import Textarea from 'primevue/textarea'
+import './EasyTextarea.scss'
 
-const model = defineModel<string>({default: ""});
+const model = defineModel<string>({ default: '' })
 
-const props = defineProps<{
-  placeholder?: string;
-  tabindex?: number;
-  invalid?: boolean;
-  rows?: number;
-  disabled?: boolean;
+defineProps<{
+  placeholder?: string
+  tabindex?: number
+  invalid?: boolean
+  rows?: number
+  disabled?: boolean
   readonly?: boolean
-}>();
+}>()
 const emit = defineEmits<{
-  (e: 'keydown.down', val?: string): void;
-  (e: 'keydown.up', val?: string): void;
-  (e: 'keydown.enter', val?: string): void;
-  (e: 'keydown.tab', val?: string): void;
-  (e: 'focus', val?: string): void;
-  (e: 'focusout', val?: string): void;
-}>();
-
-
+  (e: 'keydown.down', val: string): void
+  (e: 'keydown.up', val: string): void
+  (e: 'keydown.enter', val: string): void
+  (e: 'keydown.tab', val: string): void
+  (e: 'focus', val: string): void
+  (e: 'focusout', val: string): void
+}>()
 </script>
 
 <template>
-      <Textarea
-        v-model="model"
-        :rows="props.rows"
-        auto-resize
-        :placeholder="props.placeholder"
-        :readonly="readonly"
-        :disabled="props.disabled"
-        class="w-full"
-        :tabindex="props.tabindex"
-        :invalid="props.invalid"
-        @keydown.down="emit('keydown.down')"
-        @keydown.up="emit('keydown.up')"
-        @keydown.enter="emit('keydown.enter')"
-        @keydown.tab="emit('keydown.tab')"
-        @focus="emit('focus')"
-        @focusout="emit('focusout')"
-      />
+  <Textarea
+    v-model="model"
+    :rows="rows"
+    auto-resize
+    :placeholder="placeholder"
+    :readonly="readonly"
+    :disabled="disabled"
+    class="w-full"
+    :tabindex="tabindex"
+    :invalid="invalid"
+    @keydown.down="emit('keydown.down', model)"
+    @keydown.up="emit('keydown.up', model)"
+    @keydown.enter="emit('keydown.enter', model)"
+    @keydown.tab="emit('keydown.tab', model)"
+    @focus="emit('focus', model)"
+    @focusout="emit('focusout', model)"
+  />
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
