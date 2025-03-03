86ec69bc7a4f38e4c5f1040675457641bef88e21
azizjon <a.vorisov@easybooking.uz>
2025-03-03 14:32:06 +0500

    fix: tabs width, disabled state

--- src/components/tabs/EasyTabs.stories.ts
+++ src/components/tabs/EasyTabs.stories.ts
@@ -17,6 +17,18 @@ export const Default: Story = {
   args: {
     items: [
       {label: 'Tab 1', value: 'qq', icon: 'icon-Outline-Image', disabled: false},
+      {label: 'Tab 2', value: 'ww', icon: 'icon-Outline-Image', disabled: false},
+      {label: 'Tab 3', value: 'ee', icon: 'icon-Outline-Image', disabled: false},
+      {label: 'Tab 4', value: 'rr', icon: 'icon-Outline-Image', disabled: false},
+      {label: 'Tab 5', value: 'tt', icon: 'icon-Outline-Image', disabled: false}
+    ]
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    items: [
+      {label: 'Tab 1', value: 'qq', icon: 'icon-Outline-Image', disabled: false},
       {label: 'Tab 2', value: 'ww', icon: 'icon-Outline-Image', disabled: true},
       {label: 'Tab 3', value: 'ee', icon: 'icon-Outline-Image', disabled: false},
       {label: 'Tab 4', value: 'rr', icon: 'icon-Outline-Image', disabled: true},
