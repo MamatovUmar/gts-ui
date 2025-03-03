a91c58b1bf8376e91214396c5d659f64339f2155
azizjon <a.vorisov@easybooking.uz>
2025-03-03 18:00:03 +0500

    fix: change language

--- src/components/services-tab/ServicesTab.stories.ts
+++ src/components/services-tab/ServicesTab.stories.ts
@@ -1,26 +1,22 @@
-import type {Meta, StoryObj} from '@storybook/vue3';
-import ServicesTab from './ServicesTab.vue';
+import type { Meta, StoryObj } from '@storybook/vue3'
+import ServicesTab from './ServicesTab.vue'
 
 const meta = {
   title: 'Components/ServicesTab',
   component: ServicesTab,
   tags: ['autodocs'],
   argTypes: {
-
+    locale: {
+      options: ['ru', 'uz', 'en'],
+    }
   },
-} satisfies Meta<typeof ServicesTab>;
+} satisfies Meta<typeof ServicesTab>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-
-  }
-};
-
-export const Disabled: Story = {
-  args: {
-
-  }
-};
+    locale: 'ru',
+  },
+}
