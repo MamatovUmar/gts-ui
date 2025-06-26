4a76c5ffe40027616004212eb9a1ed3b46b0976f
umarmamatov <mamatov.umar@mail.ru>
2025-06-26 13:31:29 +0500

    feat: add cruise service type and disable hotels/tours services temporarily

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export type LocaleTypes = 'en' | 'ru' | 'uz'
 
-export type ServicesTypes = 'flights' | 'hotels' | 'railway' | 'transfers' | 'tours' | 'insurance' | 'excursions' | 'esim'
+export type ServicesTypes = 'flights' | 'hotels' | 'railway' | 'transfers' | 'tours' | 'insurance' | 'excursions' | 'esim' | 'cruise'
 
 export interface IService {
   label: Record<LocaleTypes, string>
